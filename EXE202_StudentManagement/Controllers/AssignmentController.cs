2a3039c22f27700e53524274294bdf7fbd88e7ba
QuanNT24 <quannt2403pc@gmail.com>
2025-09-05 21:13:59 +0700

    Add Task Update Status and chart for progress

---
+++ EXE202_StudentManagement/Controllers/AssignmentController.cs
@@ -0,0 +1,26 @@
+﻿using EXE202_StudentManagement.Services.Interface;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EXE202_StudentManagement.Controllers
+{
+    public class AssignmentController : Controller
+    {
+        IAssignmentService _assignmentService;
+
+        public AssignmentController(IAssignmentService assignmentService)
+        {
+            _assignmentService = assignmentService;
+        }
+
+
+        [Route("Assignment/Detail/{id}")]
+        public IActionResult Detail(int id)
+        {
+            var assignment=_assignmentService.GetAssignmentById(id);
+
+
+            return View(assignment);
+
+        }
+    }
+}
