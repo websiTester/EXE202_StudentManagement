de6a06acf131e5dc4393b615bbf3fe4a21a507fe
VuHHHE180800 <vuhhhe180800@fpt.edu.vn>
2025-09-19 02:32:07 +0700

    final update

--- EXE202_StudentManagement/Controllers/UserController.cs
+++ EXE202_StudentManagement/Controllers/UserController.cs
@@ -32,5 +32,64 @@ namespace EXE202_StudentManagement.Controllers
 
             return View(user);
         }
+        [HttpPost]
+        public async Task<IActionResult> Deactivate(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.isActive = false; // Khóa tài khoản
+
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("ListUser", "Admin");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return View("UserDetail", user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Reactivate(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.isActive = true; // Mở lại tài khoản
+
+            var result = await _userManager.UpdateAsync(user);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("ListUser", "Admin");
+            }
+
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
+
+            return View("UserDetail", user);
+        }
     }
 }
