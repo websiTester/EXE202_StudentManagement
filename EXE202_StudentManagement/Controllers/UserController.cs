01e70d4762ef0b4b6a775cf606e4ad31303fbb71
VuHHHE180800 <vuhhhe180800@fpt.edu.vn>
2025-09-19 01:18:54 +0700

    update user detail

---
+++ EXE202_StudentManagement/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using EXE202_StudentManagement.Models;
+using EXE202_StudentManagement.Repositories.Interface;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EXE202_StudentManagement.Controllers
+{
+    public class UserController : Controller
+    {
+        private RoleManager<IdentityRole> _roleManager;
+        private UserManager<User> _userManager;
+        public UserController(RoleManager<IdentityRole> roleManager,
+            UserManager<User> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        
+        public async Task<IActionResult> UserDetailAsync(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            var user = await _userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+    }
+}
