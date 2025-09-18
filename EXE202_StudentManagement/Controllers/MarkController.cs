42035cf27db85f17039045203f25b3c653421846
VuHHHE180800 <vuhhhe180800@fpt.edu.vn>
2025-09-18 15:39:48 +0700

    Mark page

---
+++ EXE202_StudentManagement/Controllers/MarkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EXE202_StudentManagement.Controllers
+{
+    public class MarkController : Controller
+    {
+        public IActionResult TeacherMark()
+        {
+            return View();
+        }
+    }
+}
